78473fbe4ebf9ea03407c19f30e485db024afe7c
Prontein <77588897+Prontein@users.noreply.github.com>
2021-09-23 19:35:17 +0500

    Lesson2

---
+++ src/main/java/ru/geekbrains/web/dtos/StringResponse.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.web.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class StringResponse {
+    private String value;
+}
