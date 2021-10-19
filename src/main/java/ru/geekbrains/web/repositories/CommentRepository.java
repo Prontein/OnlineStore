c12a27f00b54ae7eb2e81559970f58cb89b79393
Prontein <77588897+Prontein@users.noreply.github.com>
2021-10-19 17:32:05 +0500

    lesson9

--- src/main/java/ru/geekbrains/web/repositories/CommentRepository.java
+++ src/main/java/ru/geekbrains/web/repositories/CommentRepository.java
@@ -2,11 +2,11 @@ package ru.geekbrains.web.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.geekbrains.web.model.User;
+import ru.geekbrains.web.model.Comment;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<Comment> findByUsername(String username);
 }
--- src/main/java/ru/geekbrains/web/repositories/OrderRepository.java
+++ src/main/java/ru/geekbrains/web/repositories/OrderRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.geekbrains.web.model.Order;
 
-import javax.persistence.NamedEntityGraph;
 import java.util.List;
 
 @Repository
@@ -14,4 +13,8 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("select o from Order o where o.user.username = :username")
     @EntityGraph(value = "orders.for-front")
     List<Order> findAllByUsername(String username);
+
+//    @Query(nativeQuery = true, value = "select case when count(o) > 0 then true else false end " +
+//            "from orders o where o.user.user_id = :userId and o.order_id in (select i.order.order_id from order_items i where i.product.product_id = :productId)")
+//    boolean hasOrder(@Param("userId") Long userID, @Param("productId") Long productId);
 }
