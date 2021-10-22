2ccf52f2e46ff417dd8796141f7de27ebc7ccf2f
Prontein <77588897+Prontein@users.noreply.github.com>
2021-10-22 20:08:29 +0500

    Delete src directory

--- src/main/java/ru/geekbrains/web/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.web.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/geekbrains/web/repositories/OrderItemsRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.web.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.geekbrains.web.model.OrderItem;
-
-public interface OrderItemsRepository extends JpaRepository<OrderItem, Long> {
-}
