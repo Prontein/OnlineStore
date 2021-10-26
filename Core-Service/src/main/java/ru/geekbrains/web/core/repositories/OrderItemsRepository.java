0fb6a864a8135e00352074b03a1d5e96b02907cc
Prontein <77588897+Prontein@users.noreply.github.com>
2021-10-26 20:53:40 +0500

    Delete Core-Service directory

--- Core-Service/src/main/java/ru/geekbrains/web/core/repositories/OrderItemsRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.web.core.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.geekbrains.web.core.model.OrderItem;
-
-public interface OrderItemsRepository extends JpaRepository<OrderItem, Long> {
-}
--- Core-Service/src/main/resources/static/welcome/welcome.js
+++
@@ -1,3 +0,0 @@
-angular.module('storefront').controller('welcomeController', function ($scope, $http) {
-    const contextPath = 'http://localhost:8080/shop';
-});
