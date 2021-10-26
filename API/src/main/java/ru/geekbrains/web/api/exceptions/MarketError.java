c506bd1aa67a893a498e22452c9096791d313424
Prontein <77588897+Prontein@users.noreply.github.com>
2021-10-26 20:51:55 +0500

    lesson11

---
+++ API/src/main/java/ru/geekbrains/web/api/exceptions/MarketError.java
@@ -0,0 +1,45 @@
+package ru.geekbrains.web.api.exceptions;
+
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+
+public class MarketError {
+    private List<String> messages;
+    private Date date;
+
+    public MarketError() {
+    }
+
+    public List<String> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<String> messages) {
+        this.messages = messages;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public MarketError(List<String> messages) {
+        this.messages = messages;
+        this.date = new Date();
+    }
+
+    public MarketError(String message) {
+        this(List.of(message));
+    }
+
+    public MarketError(String... messages) {
+        this(Arrays.asList(messages));
+    }
+
+}
